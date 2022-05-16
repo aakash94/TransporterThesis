e95eb6c572604c713f16157e6448120fc27ce95c
Aakash Maroti <aakash.the.limitless@gmail.com>
2022-05-16 17:58:39 +0200

    Code to Run Baselines

---
+++ src/Trainer.py
@@ -0,0 +1,64 @@
+import os
+import gym
+from stable_baselines3 import A2C, DQN
+from stable_baselines3.common.monitor import Monitor
+
+
+class Trainer():
+
+    def __init__(self, atari_env=False, a2c_model=False, seed=42):
+
+        # logs_root = os.path.join(".", "logs", "wrapped")
+        logs_root = os.path.join(".", "logs", "baseline")
+        if atari_env:
+            env_name = "ALE/Enduro-v5"
+        else:
+            env_name = "CarRacing-v0"
+
+        print("Env : ", env_name)
+        self.env = Monitor(gym.make(env_name))
+        logs_root = os.path.join(logs_root, env_name)
+
+        if a2c_model:
+            print("A2C Model")
+            logs_root = os.path.join(logs_root, "A2C", "")
+
+            self.model = A2C(
+                'CnnPolicy',
+                self.env,
+                tensorboard_log=logs_root,
+                # verbose=1,
+                seed=seed)
+        else:
+            print("DQN Model")
+            logs_root = os.path.join(logs_root, "DQN", "")
+
+            self.model = DQN(
+                'CnnPolicy',
+                self.env,
+                tensorboard_log=logs_root,
+                # verbose=1,
+                seed=seed)
+
+    def train(self, total_timesteps=1000000):
+        print("Training")
+        self.model.learn(total_timesteps=total_timesteps)
+
+    def demonstrate(self, episode_count=10):
+        for ep in range(episode_count):
+            obs = self.env.reset()
+            done = False
+            while not done:
+                action, _states = self.model.predict(obs)
+                obs, rewards, done, info = self.env.step(action)
+                self.env.render()
+
+
+def main():
+    t = Trainer(a2c_model=True)
+    t.train(total_timesteps=1000000)
+    t.demonstrate()
+
+
+if __name__ == "__main__":
+    main()
