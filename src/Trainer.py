413b675f67cb5a154244275aade4c35a6379100f
aakash94 <aakash.maroti01@estudiant.upf.edu>
2022-05-22 20:00:00 +0200

    Super Suit Frame Stack

--- src/Trainer.py
+++ src/Trainer.py
@@ -2,6 +2,7 @@ import os
 import gym
 from stable_baselines3 import A2C, DQN, PPO
 from stable_baselines3.common.monitor import Monitor
+from supersuit import color_reduction_v0, frame_stack_v1
 
 PPO_str = "PPO"
 A2C_str = "A2C"
@@ -12,15 +13,19 @@ class Trainer():
 
     def __init__(self, atari_env=False, model_str=PPO_str, seed=42, verbose=0):
 
-        # logs_root = os.path.join(".", "logs", "wrapped")
-        logs_root = os.path.join(".", "logs", "baseline")
+        frame_stack_count = 5
+        experiment_folder = "ss_fs"+str(frame_stack_count)+""
+        logs_root = os.path.join(".", "logs", experiment_folder)
+        # logs_root = os.path.join(".", "logs", "baseline")
         if atari_env:
             env_name = "ALE/Enduro-v5"
         else:
             env_name = "CarRacing-v0"
 
         print("Env : ", env_name)
-        self.env = Monitor(gym.make(env_name))
+        env = gym.make(env_name)
+        env = frame_stack_v1(env, stack_size=frame_stack_count)
+        self.env = Monitor(env)
         logs_root = os.path.join(logs_root, env_name)
 
         print("Model ", model_str)
@@ -47,6 +52,7 @@ class Trainer():
                 'CnnPolicy',
                 self.env,
                 tensorboard_log=logs_root,
+                buffer_size=50000,
                 verbose=verbose,
                 seed=seed)
         else:
@@ -67,9 +73,18 @@ class Trainer():
 
 
 def main():
-    t = Trainer(model_str=PPO_str)
-    t.train(total_timesteps=1000000)
-    t.demonstrate()
+    t = Trainer(model_str=PPO_str, atari_env=True)
+    t.train(total_timesteps=5000000)
+
+    t1 = Trainer(model_str=A2C_str, atari_env=True)
+    t1.train(total_timesteps=5000000)
+
+    t2 = Trainer(model_str=DQN_str, atari_env=True)
+    t2.train(total_timesteps=5000000)
+
+    # t.demonstrate()
+    # t1.demonstrate()
+    t2.demonstrate()
 
 
 if __name__ == "__main__":
