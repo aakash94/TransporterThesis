ce5d7b0b71e5c91a1f7fb1fb40eac753d44eda92
Aakash Maroti <aakash.the.limitless@gmail.com>
2022-05-29 05:19:00 +0200

    Update FrameStackWrapper.py

--- src/FrameStackWrapper.py
+++ src/FrameStackWrapper.py
@@ -1,24 +1,43 @@
 import gym
+import numpy as np
 from collections import deque
 from stable_baselines3.common.monitor import Monitor
 
-class FrameStackWrapper(gym.Wrapper):
 
-    def __init__(self, env, frame_stack_count=4):
+class FrameStackWrapper(gym.ObservationWrapper):
+
+    def __init__(self, env, frame_stack_count=4, convert_greyscale=False):
         super().__init__(env)
+        self.convert_greyscale = convert_greyscale
         self.env = env
         self.frames = deque(maxlen=frame_stack_count)
-        print(len(self.frames))
         state = env.reset()
         for i in range(frame_stack_count):
             self.frames.append(state)
-        print(len(self.frames))
 
-    # TODO : Observation wrapper
+    def observation(self, obs):
+        if self.convert_greyscale:
+            # TODO: Merge 3 channels of colour into greyscale
+            pass
+        self.frames.append(obs)
+        state = self.frames_to_state()
+        # print("obs ", obs.shape)
+        # print("state ", type(state))
+        return state
+
+    def frames_to_state(self):
+        # print("len", len(self.frames))
+        # print("frame shape", self.frames[0].shape)
+        state = np.concatenate(list(self.frames), axis=2)
+        # print("mod state", state.shape)
+        return state
 
 
 if __name__ == "__main__":
-    env_name = "CarRacing-v0"
+    # from gym import envs
+    # print(envs.registry.all())
+    env_name = "CarRacing-v1"
     env = gym.make(env_name)
     env = Monitor(env)
     f = FrameStackWrapper(env=env)
+    s = f.reset()
