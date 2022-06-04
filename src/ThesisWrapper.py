2dc79e26bebf7fcd9918036f0f6157a2c2b0d5ab
Aakash Maroti <aakash.the.limitless@gmail.com>
2022-06-04 22:31:22 +0200

    Create ThesisWrapper.py

---
+++ src/ThesisWrapper.py
@@ -0,0 +1,54 @@
+import gym
+import gym.spaces as spaces
+import numpy as np
+from collections import deque
+import cv2
+
+
+class ThesisWrapper(gym.ObservationWrapper):
+
+    def __init__(self,
+                 env: gym.Env,
+                 history_count=4,
+                 convert_greyscale=False):
+        super().__init__(env)
+        self.history_count = history_count
+        self.convert_greyscale = convert_greyscale
+        self.frames = deque(maxlen=self.history_count)
+        state = self.reset()
+        if isinstance(self.env.observation_space, gym.spaces.Box):
+            self.observation_space = spaces.Box(low=0, high=255, shape=state.shape, dtype=np.uint8)
+
+    def observation(self, obs):
+        frame = self.operation_on_single_frame(obs=obs)
+        self.frames.append(frame)
+        state = self.operations_on_stack()
+        return state
+
+    def reset(self):
+        og_state = self.env.reset()
+        frame = self.operation_on_single_frame(obs=og_state)
+        for i in range(self.history_count - 1):
+            self.frames.append(frame)
+        obs = self.observation(obs=og_state)
+        return obs
+
+    def operation_on_single_frame(self, obs):
+        frame = obs
+        if self.convert_greyscale:
+            frame = cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY)
+        return frame
+
+    def operations_on_stack(self):
+        state = np.concatenate(list(self.frames), axis=2)
+        return state
+
+
+if __name__ == "__main__":
+    env_name = "CarRacing-v1"
+    env = gym.make(env_name, continuous=False)
+    env = ThesisWrapper(env=env)
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+    print("step shape ", observation.shape)
+    print("done checking")
