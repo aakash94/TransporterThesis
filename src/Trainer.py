71fb15572d4001b48c5e6e45c50b8439b038523e
Aakash Maroti <aakash.the.limitless@gmail.com>
2022-06-08 23:05:43 +0200

    Eval Callback

--- src/Trainer.py
+++ src/Trainer.py
@@ -2,6 +2,7 @@ import os
 import gym
 from stable_baselines3 import DQN
 from stable_baselines3.common.monitor import Monitor
+from stable_baselines3.common.callbacks import EvalCallback
 
 from ThesisWrapper import ThesisWrapper
 
@@ -13,18 +14,33 @@ class Trainer():
         frame_stack_count = 5
         experiment_folder = "gfsw" + str(frame_stack_count) + ""
         logs_root = os.path.join(".", "logs", experiment_folder)
+        model_save_path = os.path.join(".", "models", experiment_folder)
         model_str = "DQN"
         if atari_env:
             env_name = "ALE/Enduro-v5"
             env = gym.make(env_name)
+            eval_env = gym.make(env_name)
         else:
             env_name = "CarRacing-v1"
             env = gym.make(env_name, continuous=False)
+            eval_env = gym.make(env_name, continuous=False)
 
         print("Env : ", env_name)
         env = Monitor(env)
+        eval_env = Monitor(eval_env)
+
         env = ThesisWrapper(env, history_count=frame_stack_count, convert_greyscale=True)
+        eval_env = ThesisWrapper(eval_env, history_count=frame_stack_count, convert_greyscale=True)
+
+        # https://github.com/hill-a/stable-baselines/issues/1087
+        self.eval_callback = EvalCallback(eval_env,
+                                          best_model_save_path=model_save_path,
+                                          eval_freq=500,
+                                          deterministic=True,
+                                          render=False)
+
         self.env = env
+
         logs_root = os.path.join(logs_root, env_name)
 
         print("Model ", model_str)
@@ -40,7 +56,7 @@ class Trainer():
 
     def train(self, total_timesteps=1000000):
         print("Training")
-        self.model.learn(total_timesteps=total_timesteps)
+        self.model.learn(total_timesteps=total_timesteps, callback=self.eval_callback)
 
     def demonstrate(self, episode_count=10):
         for ep in range(episode_count):
