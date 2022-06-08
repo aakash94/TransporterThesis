2bb66759f14cbe2ee3022d24df8bc90d4b5e27f2
Aakash Maroti <aakash.the.limitless@gmail.com>
2022-06-08 13:29:05 +0200

    Only DQN

--- src/Trainer.py
+++ src/Trainer.py
@@ -5,19 +5,15 @@ from stable_baselines3.common.monitor import Monitor
 
 from ThesisWrapper import ThesisWrapper
 
-PPO_str = "PPO"
-A2C_str = "A2C"
-DQN_str = "DQN"
-
 
 class Trainer():
 
-    def __init__(self, atari_env=False, model_str=PPO_str, seed=42, verbose=0):
+    def __init__(self, atari_env=False, seed=42, verbose=0):
 
         frame_stack_count = 5
         experiment_folder = "gfsw" + str(frame_stack_count) + ""
         logs_root = os.path.join(".", "logs", experiment_folder)
-        # logs_root = os.path.join(".", "logs", "baseline")
+        model_str = "DQN"
         if atari_env:
             env_name = "ALE/Enduro-v5"
             env = gym.make(env_name)
@@ -34,32 +30,13 @@ class Trainer():
         print("Model ", model_str)
         logs_root = os.path.join(logs_root, model_str, "")
 
-        if model_str == PPO_str:
-            self.model = PPO(
-                'CnnPolicy',
-                self.env,
-                tensorboard_log=logs_root,
-                verbose=verbose,
-                seed=seed)
-
-        elif model_str == A2C_str:
-            self.model = A2C(
-                'CnnPolicy',
-                self.env,
-                tensorboard_log=logs_root,
-                verbose=verbose,
-                seed=seed)
-
-        elif model_str == DQN_str:
-            self.model = DQN(
-                'CnnPolicy',
-                self.env,
-                tensorboard_log=logs_root,
-                buffer_size=50000,
-                verbose=verbose,
-                seed=seed)
-        else:
-            print("Invalid Model")
+        self.model = DQN(
+            'CnnPolicy',
+            self.env,
+            tensorboard_log=logs_root,
+            buffer_size=50000,
+            verbose=verbose,
+            seed=seed)
 
     def train(self, total_timesteps=1000000):
         print("Training")
@@ -79,7 +56,7 @@ def main():
     atari_env = False
     total_timesteps = 5000000
 
-    t = Trainer(model_str=DQN_str, atari_env=atari_env)
+    t = Trainer(atari_env=atari_env)
     t.train(total_timesteps=total_timesteps)
 
 
