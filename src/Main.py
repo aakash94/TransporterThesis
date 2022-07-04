825c2275a20d34ae1370a17a81bc6b59e87b8c01
aakash <aakash.maroti01@estudiant.upf.edu>
2022-07-04 14:38:18 +0200

    Run experiments and write logs

--- src/Main.py
+++ src/Main.py
@@ -1,8 +1,71 @@
 import os
+from Trainer import Trainer, write_log
 
 
 def main():
     print("Hello")
+    atari_env = False
+    total_timesteps = 1000000
+    eval_count = 10
+    frame_stack_count = 4
+    motion = False
+    transporter = True
+
+    if motion:
+        # because not more than 2 frames are used.
+        frame_stack_count = 2
+
+    #################################
+    frame_stack_count = 1
+    motion = False
+    transporter = False
+
+    if motion:
+        # because not more than 2 frames are used.
+        frame_stack_count = 2
+
+    t = Trainer(atari_env=atari_env, frame_stack_count=frame_stack_count, motion=motion, transporter=transporter)
+    write_log(path=t.eval_path, string="1_80")
+    print("All Set")
+    t.evaluate(n_eval_episodes=eval_count)
+    t.train(total_timesteps=total_timesteps)
+    print("Done Training")
+    t.evaluate(n_eval_episodes=eval_count)
+    t.save_model()
+    ##############################################
+    frame_stack_count = 4
+    motion = False
+    transporter = False
+
+    if motion:
+        # because not more than 2 frames are used.
+        frame_stack_count = 2
+
+    t = Trainer(atari_env=atari_env, frame_stack_count=frame_stack_count, motion=motion, transporter=transporter)
+    write_log(path=t.eval_path, string="4_80")
+    print("All Set")
+    t.evaluate(n_eval_episodes=eval_count)
+    t.train(total_timesteps=total_timesteps)
+    print("Done Training")
+    t.evaluate(n_eval_episodes=eval_count)
+    t.save_model()
+    #############################################
+    frame_stack_count = 2
+    motion = True
+    transporter = False
+
+    if motion:
+        # because not more than 2 frames are used.
+        frame_stack_count = 2
+
+    t = Trainer(atari_env=atari_env, frame_stack_count=frame_stack_count, motion=motion, transporter=transporter)
+    write_log(path=t.eval_path, string="Motion_80")
+    print("All Set")
+    t.evaluate(n_eval_episodes=eval_count)
+    t.train(total_timesteps=total_timesteps)
+    print("Done Training")
+    t.evaluate(n_eval_episodes=eval_count)
+    t.save_model()
 
 
 if __name__ == "__main__":
